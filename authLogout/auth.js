20a40feda19b871082cc97ada4e49362d897edca
PedrOliveiraM <pedropucmont@gmail.com>
2024-10-02 09:58:12 -0300

    refactor: Refactor authentication and registration functionality

--- authLogout/auth.js
+++ authLogout/auth.js
@@ -1,9 +1,3 @@
-document.getElementById('btnRegister').addEventListener('click', function () {
-  var myModal = new bootstrap.Modal(document.getElementById('register-modal'));
-  myModal.show();
-});
-
-
 class User {
   constructor(name, email, password) {
     this.name = name;
@@ -11,46 +5,142 @@ class User {
     this.password = password;
   }
 }
-
 class Auth {
-
-  users = [];
-
   constructor() {
     this.users = [];
   }
 
   registerUser(name, email, password) {
-    const newUser = new User(this.name, email, password);
+    if (this.isEmailRegistered(email)) {
+      this.showError('Email já registrado.');
+      return false;
+    }
+
+    const newUser = new User(name, email, password);
     this.users.push(newUser);
+    return true;
   }
 
   loginUser(email, password) {
-    this.users.find(user => { 
-      if (user.email === email && user.password === password) {
-        return true;
-      }
-  });
+    const user = this.users.find(
+      (user) => user.email === email && user.password === password
+    );
+    return user ? true : false;
   }
 
-  isEmailRegistered(email){
-    this.users.find(user => {
-      if(user.email === email){
-        return true;
-      }
-    });
-    return false;
+  isEmailRegistered(email) {
+    const user = this.users.find((user) => user.email === email);
+    return user ? true : false;
   }
 
-  validatePassword(password, confirmPassword){
-    if(password === confirmPassword){
-      return true;
-    }
-    return false
-  }
-  
-  showError(message){
-    alert("Error !");
+  validatePassword(password, confirmPassword) {
+    return password === confirmPassword;
   }
 
+  showError(message) {
+    showToast(message, 'error');
+  }
 }
+
+const auth = new Auth();
+auth.registerUser('admin', 'admin@admin.com', 'admin');
+
+
+const btnRegister = document.getElementById('btnRegister').addEventListener('click', openModal);
+
+function openModal() {
+  var myModal = new bootstrap.Modal(document.getElementById('registerModal'));
+  myModal.show();
+}
+
+function closeModal() {
+  var myModal = bootstrap.Modal.getInstance(document.getElementById('registerModal'));
+  if (myModal) {
+      myModal.hide();
+  }
+}
+
+const btnModalSubmit = document.getElementById('btnModalSubmit');
+const modalRegisterForm = document.getElementById('modalRegisterForm');
+
+btnModalSubmit.addEventListener('click', handleFormSubmission);
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+
+  if (modalRegisterForm.checkValidity()) {
+    try {
+      const formInformation = getFormFields(modalRegisterForm);
+
+      validateForm(formInformation, auth);
+
+      auth.registerUser(formInformation['registerName'], formInformation['registerEmail'], formInformation['registerPassword']);
+      showToast('Registro realizado com sucesso', 'success ');
+      closeModal();
+
+    } catch (error) {
+      showToast(error.message, 'error');
+    }
+  } else {
+    modalRegisterForm.reportValidity();
+  }
+}
+
+function getFormFields(form) {
+  const formFields = {};
+  const formInputs = form.querySelectorAll('input');
+
+  formInputs.forEach((input) => {
+    formFields[input.id] = input.value;
+  });
+
+  return formFields;
+}
+
+function validateForm(formFields, auth) {
+  if (auth.isEmailRegistered(formFields['registerEmail'])) {
+    showToast('Esse email já existe', 'error');
+    throw new Error('Esse email já existe');
+  }
+
+  if (!auth.validatePassword(formFields['registerPassword'], formFields['registerConfirmedPassword'])) {
+    showToast('As senhas não coincidem', 'error');
+    throw new Error('As senhas não coincidem');
+  }
+}
+
+
+const loginForm = document.getElementById('loginForm');
+loginForm.addEventListener('submit', handleLogin);
+
+function handleLogin(event) {
+  event.preventDefault();
+
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  if (auth.loginUser(email, password)) {
+    window.location.href = 'main.html';
+  } else {
+    showToast('Email ou senha incorretos!', 'error');
+  }
+}
+
+function showToast(message, type) {
+  const toastElement = document.getElementById('toast');
+  const toastBody = document.querySelector('#toast .toast-body');
+  
+  // Limpa as classes de fundo antes de aplicar a nova
+  toastElement.classList.remove('bg-success', 'bg-danger');
+
+  if (type === 'error') {
+    toastElement.classList.add('bg-danger'); // Adiciona fundo vermelho para erro
+  } else {
+    toastElement.classList.add('bg-success'); // Adiciona fundo verde para sucesso
+  }
+
+  toastBody.textContent = message; // Define a mensagem do toast
+  const toast = new bootstrap.Toast(toastElement); // Cria a instância do toast
+  toast.show(); // Exibe o toast
+}
+
